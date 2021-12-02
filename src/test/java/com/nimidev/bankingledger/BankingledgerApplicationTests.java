3202ebfaac1b8e7147b3a4210f1c88f3fc6026f6
mhashendre <malithhashendredesilva@gmail.com>
2021-12-02 19:03:20 +0530

    Initial commit

---
+++ src/test/java/com/nimidev/bankingledger/BankingledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimidev.bankingledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingledgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
